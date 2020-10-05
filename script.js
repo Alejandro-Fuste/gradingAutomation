e315c1cd90e5f2f32baef213f8d341a40b7ea806
Alejandro F <afuste7@gmail.com>
2020-10-05 14:21:59 -0400

    change default viewport to null to get viewport to fit the entire window

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 (async () => {
 	// Go the Launchpad website (https://launchpad.classlink.com/ocps)
 
-	const browser = await puppeteer.launch({ headless: false, slowMo: 100 });
+	const browser = await puppeteer.launch({ headless: false, slowMo: 100, defaultViewport: null });
 	const page = await browser.newPage();
 	await page.goto('https://launchpad.classlink.com/ocps');
 
@@ -21,9 +21,14 @@ require('dotenv').config();
 
 	// Double click on the Canvas button
 
+	// await Promise.all([
+	// 	page.waitForXPath('/html/body/app-root/secure/div/div/div/app-apps-container/application[3]'),
+	// 	page.mouse.click(718, 141, [ { clickCount: 2 } ])
+	// ]);
+
 	await page.waitForXPath('/html/body/app-root/secure/div/div/div/app-apps-container/application[3]');
 
-	await page.mouse.click(683, 141, [ { clickCount: 2 } ]);
+	await page.mouse.click(718, 141, [ { clickCount: 2 } ]);
 
 	// Single click on the class
 
@@ -31,17 +36,21 @@ require('dotenv').config();
 	// 	{ timeout: 0 }
 	// ]);
 
-	// await page.waitForNavigation({ waitUntil: 'networkidle2' });
+	// await page.waitForSelector('#application');
+
+	// await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
 
 	// await page.mouse.move(257, 454);
 
 	// await page.mouse.click(257, 454, [ { clickCount: 2 } ]);
 	// await page.waitForSelector('a[href="/courses/1221491"]');
 
-	await Promise.all([
-		page.waitForNavigation(), // The promise resolves after navigation has finished
-		page.click('a[href="/courses/1221491"]', [ { clickCount: 2 } ]) // Clicking the link will indirectly cause a navigation
-	]);
+	// await Promise.all([
+	// 	page.waitForSelector('#application'), // The promise resolves after navigation has finished
+	// 	console.log('Step after navigation'),
+	// 	page.mouse.click(170, 270, [ { clickCount: 2 } ])
+	// 	// page.click('[href=/courses/1221491]', [ { clickCount: 2 } ]) // Clicking the link will indirectly cause a navigation
+	// ]);
 
 	// await page.click('a[href="/courses/1221491"]', [ { clickCount: 2 } ]);
 
@@ -60,3 +69,7 @@ Code to get mouse coordinates:
 window.addEventListener('mousemove', (e) => console.log(`x: ${e.x} | y: ${e.y}`));
 
 */
+
+// #application > #wrapper > #not_right_side > #content-wrapper > #content > #dashboard > .DashboardCard_Container > .ic-DashboardCard__box > .ic-DashboardCard > .ic-DashboardCard__header > a[href = '/courses/1221491'];
+
+// #wrapper > #not_right_side > #content-wrapper > #content > #dashboard > .DashboardCard_Container > .ic-DashboardCard__box > .ic-DashboardCard > .ic-DashboardCard__header > a[href = "/courses/1221491"] > ic-DashboardCard__header-title ellipsis > span
