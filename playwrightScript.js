34f266e5b9f54cf22a0c8abc4c385e2f118300f7
Alejandro F <afuste7@gmail.com>
2020-10-08 15:13:40 -0400

    changed selector in waitForSelector method

--- playwrightScript.js
+++ playwrightScript.js
@@ -23,22 +23,17 @@ require('dotenv').config();
 
 	// Go to gradebook of First Aid Class by using url in goto method
 
-	await Promise.all([
-		page.goto('https://ocps.instructure.com/courses/1221491/gradebook'),
-		page.waitForNavigation() // The promise resolves after navigation has finished
-	]);
+	await page.goto('https://ocps.instructure.com/courses/1221491/gradebook');
 
 	// Find ungraded all assignments
 
-	await Promise.all([
-		page.waitForLoadState(),
-		page.waitForSelector('.slick-cell.b1.f1.assignment.assignment_15960887')
-	]);
+	await page.waitForSelector('#application');
 
-	await page.type('.slick-cell.b1.f1.assignment.assignment_15960887', '1000');
+	await page.$$('#application');
 
-	// await page.waitForSelector('.slick-cell.b15.f15.assignment.assignment_15815773');
 	// await page.click('.slick-cell.b15.f15.assignment.assignment_15815773');
 
 	// Enter a grade of "100" for all assignments that are not yet graded
 })();
+
+// '.ui-widget-content.slick-row.even.first-row.student_2344793 .slick-cell.b1.f1.assignment.assignment_15960887 .Grid__GradeCell.gradebook-cell .Grid__GradeCell__Content .Grade'
